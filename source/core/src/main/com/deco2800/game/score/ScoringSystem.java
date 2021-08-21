d94862abcdb825e9f7fed182c5a21f7a55c850b5
Bohan-is-in-the-zone <xubohan2000_sjz@163.com>
2021-08-21 09:59:54 +0800

    The starting point of scoring system

---
+++ source/core/src/main/com/deco2800/game/score/ScoringSystem.java
@@ -0,0 +1,24 @@
+package com.deco2800.game.score;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**This class is written by team 9.
+ * This class handles the scoring system*/
+public class ScoringSystem {
+
+    //A global variable, indicate the gaming time in seconds
+    private double seconds = 0;
+
+    //Calender can help us to use the current time
+    private Calendar calender;
+
+    //SimpleDateFormat is to help us to set the format of the time.
+    private SimpleDateFormat clockFormat;
+
+    /**
+     * Record the gaming time in seconds.*/
+    public void gameClock() {
+
+    }
+}
