68b6379f5fdf5eb327d0770bbb20ce9b969ebbd8
ThatTechedGuy <thattechedguy@gmail.com>
2021-08-29 03:11:40 +0530

    chore(achievements): removed redundant comments

--- source/core/src/main/com/deco2800/game/entities/configs/AchievementConfigs.java
+++ source/core/src/main/com/deco2800/game/entities/configs/AchievementConfigs.java
@@ -6,14 +6,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AchievementConfigs {
-//    public BaseAchievementConfig veteranBronze = new BaseAchievementConfig();
-//    public BaseAchievementConfig veteranSilver = new BaseAchievementConfig();
-//    public BaseAchievementConfig veteranGold = new BaseAchievementConfig();
-//    public BaseAchievementConfig toolMasterBronze = new BaseAchievementConfig();
-//    public BaseAchievementConfig toolMasterSilver = new BaseAchievementConfig();
-//    public BaseAchievementConfig toolMasterGold = new BaseAchievementConfig();
-//    public BaseAchievementConfig gameBreakerBronze = new BaseAchievementConfig();
-//    public BaseAchievementConfig gameBreakerSilver = new BaseAchievementConfig();
-//    public BaseAchievementConfig gameBreakerGold = new BaseAchievementConfig();
     public List<BaseAchievementConfig> achievements = new ArrayList<>();
 }
