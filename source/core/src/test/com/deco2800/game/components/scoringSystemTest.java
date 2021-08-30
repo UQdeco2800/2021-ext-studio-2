cbbb4ed732162de97bd667d5178b3377342ba0ce
Bohan-is-in-the-zone <xubohan2000_sjz@163.com>
2021-08-30 16:04:57 +0800

    create a junit test class

---
+++ source/core/src/test/com/deco2800/game/components/scoringSystemTest.java
@@ -0,0 +1,11 @@
+package com.deco2800.game.components;
+
+import com.deco2800.game.components.score.ScoringSystem;
+import com.deco2800.game.components.score.ScoringSystemV1;
+
+//JUnit test for the scoringSystem by team 9
+//Because the feature we did can be easily tested by running the project.
+//There are only limited tests are necessary.
+public class scoringSystemTest {
+    ScoringSystemV1 clock = new ScoringSystemV1();
+}
