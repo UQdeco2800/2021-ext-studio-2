5f9ec13ab2f8ce70f647bc887063086d0a5322d3
butcanudothis <akshayvarma.varma@gmail.com>
2021-09-11 00:08:10 +0530

    feat(achievements screen): temp background added

--- source/core/src/main/com/deco2800/game/components/achievements/screen/ChapterDisplay.java
+++ source/core/src/main/com/deco2800/game/components/achievements/screen/ChapterDisplay.java
@@ -1,16 +1,17 @@
 package com.deco2800.game.components.achievements.screen;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.deco2800.game.files.GameChapters;
+import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
 
 public class ChapterDisplay extends UIComponent {
@@ -24,11 +25,12 @@ public class ChapterDisplay extends UIComponent {
 
     private void openChapter(GameChapters.Chapter chapter) {
         // Display gui
+        ServiceLocator.getResourceService().loadTextures(new String[]{"images/achievements/bg.png"});
         Dialog dialog = new Dialog("Chapter "+ chapter.id, skin);
         dialog.setModal(true);
         dialog.setMovable(true);
         dialog.setResizable(true);
-
+        dialog.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture("images/achievements/bg.png"))));
         dialog.padTop(50).padBottom(50);
 
         Label story = new Label(chapter.content, new Label.LabelStyle(new BitmapFont(), Color.BROWN));
