0d77ecb3e59e3cfc1f4bc9fc4b5383939d54b8a9
pranavdhoolia <dhoolia.pranav@gmail.com>
2021-09-14 11:06:48 +0530

    declare score related tests for 'GameRecords'

---
+++ source/core/src/test/com/deco2800/game/files/GameRecordsTest.java
@@ -0,0 +1,30 @@
+package com.deco2800.game.files;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameRecordsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getScoreByGame() {
+    }
+
+    @Test
+    void getAllScores() {
+    }
+
+    @Test
+    void getHighestScores() {
+    }
+}
