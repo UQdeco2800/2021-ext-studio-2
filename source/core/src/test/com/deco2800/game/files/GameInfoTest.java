5bc379762c72c3e5f0e3bf74c9294bc7ae985a76
ThatTechedGuy <thattechedguy@gmail.com>
2021-09-30 22:18:55 +0530

    test(Achievement): added javadoc, minor improvements to existing achievemments, minor ui fix in achievements screen

--- source/core/src/test/com/deco2800/game/files/GameInfoTest.java
+++ source/core/src/test/com/deco2800/game/files/GameInfoTest.java
@@ -18,20 +18,26 @@ public class GameInfoTest {
     /**
      * Change the value of a private static final variable
      *
+     * NOTE: Sensitive operations will only work <= JDK13
+     *
      * @param field    the private static final variable field
      * @param newValue the value to replace
      */
     static void setFinalStatic(Field field, Object newValue) {
+        //
         try {
+            // Make the private field accessible
             field.setAccessible(true);
 
+            // Turn off the final modifier so that the field can be manipulated
             Field modifiersField = Field.class.getDeclaredField("modifiers");
             modifiersField.setAccessible(true);
             modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
+            // Set the new value to this now non final public field
             field.set(null, newValue);
         } catch (Exception ignored) {
-
+            // SecurityException and other similar exceptions ignored
         }
     }
 
@@ -41,23 +47,36 @@ public class GameInfoTest {
         // Changing the JSON file path so that the original game is not affected
         setFinalStatic(GameInfo.class.getDeclaredField("path"),
                 "test/files/gameInfoTest.json");
+        // Changing the JSON file path so that the original game is not affected
         setFinalStatic(GameInfo.class.getDeclaredField("location"), FileLoader.Location.LOCAL);
     }
 
 
+    /**
+     * Check if the game count is being incremented properly
+     */
     @Test
     void incrementsGameCountByOne() {
         int count = GameInfo.getGameCount();
+        // Increment the game count
         GameInfo.incrementGameCount();
         int newCount = GameInfo.getGameCount();
+        // The difference in current and original game count should now be 1
         assertEquals(newCount, count + 1);
     }
 
+    /**
+     * Check if the game count is 0 by default
+     */
     @Test
-    void returnsZeroGameCountsIfNoPrevious() throws Exception {
-        setFinalStatic(GameInfo.class.getDeclaredField("path"),
-                "test/files/missing.json");
-        int count = GameInfo.getGameCount();
-        //assertEquals(count, 0);
+    void returnsZeroGameCountsIfNoPrevious() {
+        try {
+            // The game count should be zero by default, or if the JSON file is missing
+            setFinalStatic(GameInfo.class.getDeclaredField("path"),
+                    "test/files/missingGameCount.json");
+            assertEquals(GameInfo.getGameCount(), 0);
+        } catch (Exception ignored){
+            // Ignore field change exceptions
+        }
     }
 }
