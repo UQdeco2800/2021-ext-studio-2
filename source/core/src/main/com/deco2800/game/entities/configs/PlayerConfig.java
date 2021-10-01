3111712c3c421d80022410a617824aa8d9d69aa2
Rohith <rohithkp99@gmail.com>
2021-10-01 16:40:31 +0530

    Update PlayerConfig.java

--- source/core/src/main/com/deco2800/game/entities/configs/PlayerConfig.java
+++ source/core/src/main/com/deco2800/game/entities/configs/PlayerConfig.java
@@ -6,4 +6,5 @@ package com.deco2800.game.entities.configs;
 public class PlayerConfig extends BaseEntityConfig  {
   public int gold = 1;
   public String favouriteColour = "none";
+  public String attire = "original";
 }
