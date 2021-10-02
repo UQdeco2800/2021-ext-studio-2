b8a9834a2beff42e9ab2c090fc6f0a3650d080f8
Rohith <rohithkp99@gmail.com>
2021-10-02 16:48:47 +0530

    Create MPCConfig.java

---
+++ source/core/src/main/com/deco2800/game/files/MPCConfig.java
@@ -0,0 +1,14 @@
+package com.deco2800.game.files;
+
+import com.deco2800.game.components.achievements.AchievementsStatsComponent;
+import com.deco2800.game.entities.configs.PlayerConfig;
+import com.deco2800.game.services.ServiceLocator;
+
+import java.io.File;
+
+import static com.deco2800.game.files.FileLoader.Location.EXTERNAL;
+
+public class MPCConfig {
+
+   
+}
