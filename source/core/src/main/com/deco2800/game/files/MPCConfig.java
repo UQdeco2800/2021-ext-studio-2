2885e0e837972a795e39d7aeb5cb22297ae5213a
Rohith <rohithkp99@gmail.com>
2021-10-02 16:49:28 +0530

    Read and store config values

--- source/core/src/main/com/deco2800/game/files/MPCConfig.java
+++ source/core/src/main/com/deco2800/game/files/MPCConfig.java
@@ -10,5 +10,22 @@ import static com.deco2800.game.files.FileLoader.Location.EXTERNAL;
 
 public class MPCConfig {
 
-   
+    private static final String ROOT_DIR = "DECO2800Game";
+    private static final String CONFIG_FILE = "mpc.json";
+    private static final String path = ROOT_DIR + File.separator + CONFIG_FILE;
+
+    /**
+     * Stores the current values into a JSON file
+     */
+    public void readValues() {
+        PlayerConfig values = getValues();
+    }
+
+    
+
+    public static PlayerConfig getValues() {
+        PlayerConfig values =  FileLoader.readClass(PlayerConfig.class, path, EXTERNAL);
+        return values != null ? values : new PlayerConfig();
+
+    }
 }
