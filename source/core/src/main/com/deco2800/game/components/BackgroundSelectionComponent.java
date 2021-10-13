1a1ccdc21ed36e6b6967a5b75c2313c172e0f8ee
butcanudothis <akshayvarma.varma@gmail.com>
2021-10-13 18:16:05 +0530

    feat(music Selector): implemented dialog in reusable BackgroundSelectionComponent for music chooser GUI

--- source/core/src/main/com/deco2800/game/components/BackgroundSelectionComponent.java
+++ source/core/src/main/com/deco2800/game/components/BackgroundSelectionComponent.java
@@ -3,18 +3,17 @@ package com.deco2800.game.components;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
 
 public class BackgroundSelectionComponent extends UIComponent {
-    public static final String ON_CLICK = "OnClick";
     private final String[] textures = {"images/settings/musicSelectionDialog.png",
-            "images/settings/musicSelectionButton.png"};
+            "images/settings/musicSelectionButton.png", "images/settings/radioButtonOn.png", "images/settings/radioButtonOff.png"};
     private Table musicSelectionTable;
+    private Dialog dialog;
 
     @Override
     public void create() {
@@ -29,15 +28,13 @@ public class BackgroundSelectionComponent extends UIComponent {
     }
 
     private void addActors() {
-        Image img = new Image(ServiceLocator.getResourceService()
-                .getAsset("images/settings/musicSelectionDialog.png", Texture.class));
-
         Image musicImg = new Image(ServiceLocator.getResourceService()
                 .getAsset("images/settings/musicSelectionButton.png", Texture.class));
         ImageButton musicImgBtn = new ImageButton(musicImg.getDrawable());
         musicImgBtn.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
+                showMusicDialog();
             }
         });
 
@@ -47,6 +44,80 @@ public class BackgroundSelectionComponent extends UIComponent {
         musicSelectionTable.add(musicImgBtn);
 
         stage.addActor(musicSelectionTable);
+    }
+
+    private void showMusicDialog() {
+        dialog = new Dialog("", skin);
+        dialog.setModal(true);
+        dialog.setMovable(false);
+        dialog.setResizable(true);
+
+        Image dialogBackgroundImg = new Image(ServiceLocator.getResourceService()
+                .getAsset("images/settings/musicSelectionDialog.png", Texture.class));
+
+        dialog.setBackground(dialogBackgroundImg.getDrawable());
+
+        dialog.pad(50).padTop(120);
+
+        Label heading = new Label("Select background music", skin);
+        heading.setFontScale(1.5f);
+
+        dialog.getContentTable().add(heading).expandX().row();
+        dialog.getContentTable().add(getTrackTable("Track1 placeholder text ", false)).padTop(70).row();
+        dialog.getContentTable().add(getTrackTable("Track2 placeholder text ", true)).padTop(30).row();
+
+        dialog.getButtonTable().add(renderCloseButton()).size(50, 50).row();
+
+        dialog.show(stage);
+
+
+    }
+
+    private Table getTrackTable(String trackName, boolean selected) {
+        Table table = new Table();
+        table.add(getRadioButton(selected)).size(50, 50);
+        table.add(getLabel(trackName)).width(350);
+
+        return table;
+    }
+
+
+    private ImageButton getRadioButton(boolean status) {
+        Image radioButton;
+        if (status) {
+            radioButton = new Image(ServiceLocator.getResourceService()
+                    .getAsset("images/settings/radioButtonOn.png", Texture.class));
+        } else {
+            radioButton = new Image(ServiceLocator.getResourceService()
+                    .getAsset("images/settings/radioButtonOff.png", Texture.class));
+        }
+
+        return new ImageButton(radioButton.getDrawable());
+    }
+
+    private Label getLabel(String text) {
+        Label trackLabel = new Label(text, skin);
+        trackLabel.setFontScale(1.1f);
+        trackLabel.setWrap(true);
+        trackLabel.setAlignment(Align.topLeft);
+        return trackLabel;
+    }
+
+    /**
+     * The close button which triggers a task to close the dialog.
+     *
+     * @return closeButton image
+     */
+    private ImageButton renderCloseButton() {
+        Image crossButtonImg = new Image(new Texture("images/achievements/crossButton.png"));
+        ImageButton closeButton = new ImageButton(crossButtonImg.getDrawable());
+        closeButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                dialog.hide();
+            }
+        });
+        return closeButton;
     }
 
     @Override
