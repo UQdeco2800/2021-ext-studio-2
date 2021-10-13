9467b2b84a92f2d1a0c66e9041a4fce020dd47f3
Rohith Palakirti <rohithkp99@gmail.com>
2021-10-13 14:11:26 +0530

    Resize MPC

--- source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
@@ -167,8 +167,9 @@ public class PlayerFactory {
 
 
         PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
-        player.getComponent(ColliderComponent.class).setDensity(1.5f);
         player.getComponent(TextureRenderComponent.class).scaleEntity();
+        player.setScale(2.5f,2.5f);
+        player.getComponent(ColliderComponent.class).setDensity(1.5f);
         player.getEvents().trigger("startMPCAnimation");
         return player;
     }
