5cfd2199705c4dad6a03deddfd84f5c9dc8897d3
pranavdhoolia <dhoolia.pranav@gmail.com>
2021-10-17 23:58:28 +0530

    add tests for background sounds

--- source/core/src/test/com/deco2800/game/components/BackgroundSoundComponentTest.java
+++ source/core/src/test/com/deco2800/game/components/BackgroundSoundComponentTest.java
@@ -1,7 +1,32 @@
 package com.deco2800.game.components;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BackgroundSoundComponentTest {
+    @BeforeEach
+    void setUp() {
 
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+
+    @Test
+    void name() {
+
+    }
 }
