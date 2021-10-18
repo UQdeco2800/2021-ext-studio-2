93f72581a871d9878817431121df90606a223415
yyfyyfstudy1 <88156048+yyfyyfstudy1@users.noreply.github.com>
2021-10-18 23:29:43 +0800

    update spaceship animation

--- source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
@@ -124,7 +124,7 @@ public class NPCFactory {
                                 .getAsset("images/spaceship.atlas", TextureAtlas.class));
 
         animator.addAnimation("spaceship", 0.2f, Animation.PlayMode.LOOP);
-        animator.addAnimation("spaceship_disappear", 0.2f, Animation.PlayMode.NORMAL);
+        animator.addAnimation("spaceship_disappear", 0.13f, Animation.PlayMode.NORMAL);
 
         spaceship
                 .addComponent(new PhysicsComponent())
