b43501c2fc91878159ac3c02f9e895f6925ed9c3
pranavdhoolia <dhoolia.pranav@gmail.com>
2021-10-19 02:25:37 +0530

    play sound

--- source/core/src/test/com/deco2800/game/components/BackgroundSoundComponentTest.java
+++ source/core/src/test/com/deco2800/game/components/BackgroundSoundComponentTest.java
@@ -1,10 +1,27 @@
 package com.deco2800.game.components;
 
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.deco2800.game.components.obstacle.ObstacleEventHandler;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.physics.PhysicsService;
+import com.deco2800.game.physics.components.HitboxComponent;
+import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.services.ResourceService;
+import com.deco2800.game.services.ServiceLocator;
 import org.junit.jupiter.api.*;
 
+import static com.deco2800.game.entities.factories.NPCFactory.createSpaceShip;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class BackgroundSoundComponentTest {
+
+    private static final String backgroundMusic= "sounds/temp_bgm.wav";
+    private static final float volume = 0.3f;
+    private static final boolean looping = true;
+
     @BeforeEach
     void setUp() {
 
@@ -17,7 +34,7 @@ class BackgroundSoundComponentTest {
 
     @BeforeAll
     static void beforeAll() {
-
+        ServiceLocator.registerResourceService(new ResourceService());
     }
 
     @AfterAll
@@ -25,8 +42,23 @@ class BackgroundSoundComponentTest {
 
     }
 
-    @Test
-    void name() {
+    Entity createEntity1() {
+        Entity entity =
+                new Entity();
+        return entity;
+    }
 
+    @Test
+    void playsound() {
+        Sound sound0 = mock(Sound.class);
+        Entity en = createEntity1();
+
+        Sound sound = mock(Sound.class);
+
+        en.addComponent(new Component());
+        en.create();
+        en.getEvents().trigger("spaceshipSound");
+
+//        verify(sound).play(1f, 0.6f, 0);
     }
 }
